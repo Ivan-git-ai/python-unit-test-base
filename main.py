14268327c34479e1ac1ab4c9373c8f404ac26d6b
Ivan-git-ai <eeivan2209@gmail.com>
2024-11-12 18:02:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,5 +5,9 @@ def min_of_three_vars(a, b, c):
         return b
     elif c < a and c < b:
         return c
-
+        
+def main():
+    print(min_of_threee_vars(-3,10,100))
     
+if __name__ == "__main__":
+    main()
